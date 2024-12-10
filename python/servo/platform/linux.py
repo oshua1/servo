0221d97d4df0867ded49e0b847c0d3b7bc08e160
oshua1 <oshua-github@oms.linux-one.org>
2024-12-10 14:54:02 +0100

    Add missing dev packages to list of needed packages for Debian/Ubuntu
    Signed-off-by: oshua1 <oshua-github@oms.linux-one.org>

--- python/servo/platform/linux.py
+++ python/servo/platform/linux.py
@@ -26,8 +26,10 @@ from .build_target import BuildTarget
 # 4. paste into https://github.com/servo/servo/wiki/Building#debian-based-distributions
 APT_PKGS = [
     'build-essential', 'ccache', 'clang', 'cmake', 'curl', 'g++', 'git',
-    'gperf', 'libdbus-1-dev', 'libfreetype6-dev', 'libgl1-mesa-dri',
+    'gperf', 'libdbus-1-dev',
+    'libfontconfig-dev', 'libfreetype6-dev', 'libgl1-mesa-dri',
     'libgles2-mesa-dev', 'libglib2.0-dev',
+    'libgstreamer1.0-dev',
     'gstreamer1.0-plugins-good', 'libgstreamer-plugins-good1.0-dev',
     'gstreamer1.0-plugins-bad', 'libgstreamer-plugins-bad1.0-dev',
     'gstreamer1.0-plugins-ugly',
